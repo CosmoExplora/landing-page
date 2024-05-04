7338e1bf3404b38f177502f17ea88e7426cc075c
FreyNell <freybalaguera@gmail.com>
2024-05-04 20:56:17 +0000

    update

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Your code here
-    console.log("Document is ready!");
-    var videoPlayer = document.getElementById("ytplayer");
-    videoPlayer.click();
-});
