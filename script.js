9a9aa3725caca8adfc146c963fd4ac9c1a9e3466
FreyNell <freybalaguera@gmail.com>
2024-05-04 18:06:16 +0000

    embed video

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    console.log("Document is ready!");
+    var videoPlayer = document.getElementById("ytplayer");
+    videoPlayer.click();
+});
